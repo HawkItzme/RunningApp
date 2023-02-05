68edf78e542a63a5c62ff3c03e1dca1eb2429c94
Hawk Itzme <smquasim016@gmail.com>
2023-02-05 18:33:27 +0530

    Fragment added, dependencies updated in gradle, Dependency Injection code implemented

---
+++ app/src/main/java/com/example/runningapp/repositories/MainRepository.kt
@@ -0,0 +1,29 @@
+package com.example.runningapp.repositories
+
+import com.example.runningapp.db.Run
+import com.example.runningapp.db.RunDAO
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    val runDao: RunDAO
+){
+    suspend fun insertRun(run: Run) = runDao.insertRun(run)
+
+    suspend fun deleteRun(run: Run) = runDao.deleteRun(run)
+
+    fun getAllRunsSortedByDate() = runDao.getAllRunSortedByDate()
+
+    fun getAllRunsSortedByDistance() = runDao.getAllRunSortedByDistance()
+
+    fun getAllRunsSortedByTimeInMillis() = runDao.getAllRunSortedByTimeInMillis()
+
+    fun getAllRunsSortedByCaloriesBurned() = runDao.getAllRunSortedByCaloriesBurned()
+
+    fun getTotalAvgSpeed() = runDao.getTotalAvgSpeed()
+
+    fun getTotalDistance() = runDao.getTotalDistance()
+
+    fun getTotalCaloriesBuned() = runDao.getTotalCaloriesBurned()
+
+    fun getTotalTimeInMillis() = runDao.getTotalTimeInMillis()
+}
---
+++ app/src/main/java/com/example/runningapp/ui/viewModel/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.runningapp.ui.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.runningapp.repositories.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    val mainRepository: MainRepository
+): ViewModel() {
+}
