768911294140f7dd94eddc0fa6ce2942c611caa4
Hawk Itzme <smquasim016@gmail.com>
2023-02-08 23:53:48 +0530

    Drawing  running track code 1 added

--- app/src/main/java/com/example/runningapp/services/TrackingService.kt
+++ app/src/main/java/com/example/runningapp/services/TrackingService.kt
@@ -7,11 +7,13 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
 import com.example.runningapp.R
 import com.example.runningapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.runningapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
@@ -20,12 +22,31 @@ import com.example.runningapp.other.Constants.ACTION_STOP_SERVICE
 import com.example.runningapp.other.Constants.NOTIFICATION_CHANNEL_ID
 import com.example.runningapp.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.example.runningapp.other.Constants.NOTIFICATION_ID
+import com.example.runningapp.other.TrackingUtility
 import com.example.runningapp.ui.MainActivity
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
 
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 class TrackingService : LifecycleService() {
 
     val isFirstRun = true
+
+    companion object{
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues(){
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when(it.action){
@@ -50,7 +71,47 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun updateLocationTracking(isTracking: Boolean){
+        if (isTracking){
+            if (TrackingUtility.hasLocationPermissions(this)){
+                val request = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY)
+            }
+            }
+    }
+
+    val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(result: LocationResult?) {
+            if (result != null) {
+                super.onLocationResult(result)
+            }
+            if (isTracking.value!!){
+                result?.locations?.let { locations ->
+                    for (location in locations){
+                        addPathPoint(location)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?){
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    }?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     private fun startForegroundService(){
+        addEmptyPolyline()
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
                 as NotificationManager
 
